48cf9257bb652b08904e913d6005fd4711f27a97
geermc4 <geermc4@gmail.com>
2013-03-26 15:33:54 -0700

    fix migration

--- db/migrate/20130322202740_add_product_id_to_option_value.rb
+++ db/migrate/20130322202740_add_product_id_to_option_value.rb
@@ -1,4 +1,4 @@
-class AddProductIdToOptionType < ActiveRecord::Migration
+class AddProductIdToOptionValue < ActiveRecord::Migration
   def change
     add_column :spree_option_values, :variant_id, :int
   end
