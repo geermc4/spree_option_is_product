b51099fabd3d34414ec392d293e6f5fb4ba435a0
Ramón Roche <mrpollo@gmail.com>
2013-03-27 14:09:32 -0700

    Fixed columns and added rows

--- app/overrides/add_product_based_checkbox.rb
+++ app/overrides/add_product_based_checkbox.rb
@@ -1,4 +1,14 @@
 Deface::Override.new(
+  :virtual_path       =>  "spree/admin/option_types/_form",
+  :name               =>  "add_product_based_checkbox_classes_remove_alpha",
+  :remove_from_attributes  =>  "[data-hook='admin_option_type_form_fields'] div.columns",
+  :attributes         =>  {:class => 'eight'})
+Deface::Override.new(
+  :virtual_path       =>  "spree/admin/option_types/_form",
+  :name               =>  "add_product_based_checkbox_classes",
+  :add_to_attributes  =>  "[data-hook='admin_option_type_form_fields'] div.columns",
+  :attributes         =>  {:class => 'seven'})
+Deface::Override.new(
   :virtual_path  => "spree/admin/option_types/_form",
   :name          => "add_product_based_checkbox",
   :insert_bottom => "[data-hook='admin_option_type_form_fields'],#admin_option_type_form_fields[data-hook]",
