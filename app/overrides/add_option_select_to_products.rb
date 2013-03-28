0bf2793a8f2515e94e608a0a41542df63d91eb6f
Ramón Roche <mrpollo@gmail.com>
2013-03-27 18:28:36 -0700

    place inside content desc like other options

--- app/overrides/add_option_select_to_products.rb
+++ app/overrides/add_option_select_to_products.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   :virtual_path => "spree/products/show",
   :name         => "add_option_select_to_products",
-  :insert_bottom => "[data-hook='product_show'],#cart_form[product_show]",
+  :insert_bottom => "[data-hook='product_right_part_wrap']",
   :partial      => "spree/products/options_display"
 )
