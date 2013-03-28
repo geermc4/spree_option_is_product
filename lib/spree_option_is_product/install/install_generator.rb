31373b649f56661be8872b7aaf36cef77e1822a9
Ramón Roche <mrpollo@gmail.com>
2013-03-27 17:29:37 -0700

    install stylesheets

--- lib/spree_option_is_product/install/install_generator.rb
+++ lib/spree_option_is_product/install/install_generator.rb
@@ -3,7 +3,11 @@ module SpreeOptionIsProduct
     class InstallGenerator < Rails::Generators::Base
       def add_javascripts
         append_file "app/assets/javascripts/admin/all.js", "//= require admin/option_types\n"
+        append_file "app/assets/javascripts/admin/all.js", "//= require fancybox\n"
       end
+      def add_stylesheets
+        append_file "app/assets/stylesheets/admin/all.css", "*= admin/spree_option_is_product\n"
+        append_file "app/assets/stylesheets/admin/all.css", "*= fancybox\n"
       def add_migrations
         run 'rake railties:install:migrations FROM=spree_option_is_product'
       end
