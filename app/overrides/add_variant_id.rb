c35725f6fcfe6d128233ce90078800a567f1a726
geermc4 <geermc4@gmail.com>
2013-04-04 14:30:40 -0700

    show field only for products with product options

--- app/overrides/add_variant_id.rb
+++ app/overrides/add_variant_id.rb
@@ -2,5 +2,5 @@ Deface::Override.new(
   :virtual_path  => "spree/admin/option_types/_option_value_fields",
   :name          => "add_variant_id",
   :insert_bottom => "[data-hook='option_value'],#option_value[data-hook]",
-  :text          => "<%= f.hidden_field :variant_id %>"
+  :text          => "<% if @product.option_types.collect(&:product_based).include?(true) %> <%= f.hidden_field :variant_id %> <% end %>"
 )
