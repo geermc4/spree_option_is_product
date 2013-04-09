9907e106f1b8aadf9ee9f5bc2eef7e9a1a03b320
geermc4 <geermc4@gmail.com>
2013-04-09 13:45:12 -0700

    creating separate lineitem for multiple bundles, fixes #1

--- app/controllers/orders_controller_decorator.rb
+++ app/controllers/orders_controller_decorator.rb
@@ -1,32 +1,19 @@
 Spree::OrdersController.class_eval do
-  append_after_filter :set_line_item_parent, :only => :populate
   append_after_filter :add_children, :only => :populate
   append_after_filter :update_children, :only => :update
 
   private
-  def set_line_item_parent
-    unless params["product_options"].blank?
-      @order = current_order
-      params["product_options"].each do |k,v|
-        next if v.blank?
-        variant = Spree::OptionValue.find(v).variant
-        line_item = @order.line_items.where(:variant_id => variant.id).first
-        unless line_item.blank?
-          parent = @order.line_items.where(:variant_id => params["variants"].keys.first).last
-          line_item.parent_id = parent.id
-          line_item.save
-        end
-      end
-    end
-  end
 
   def add_children
     unless params["product_options"].blank?
+      parent = current_order.find_line_item_by_variant(Spree::Variant.find(params["variants"].first[0]))
+
       params["product_options"].each do |k,v|
         next if v.blank?
         ov = Spree::OptionValue.find(v)
         li = current_order.add_variant(ov.variant,1,current_currency)
         li.price = ov.special_price || ov.variant.price
+        li.parent_id = parent.id
         li.save!
       end
     end
