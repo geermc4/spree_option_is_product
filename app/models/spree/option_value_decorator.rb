6ec9a78d123ca6170ac57bc3c3c145fa0d9a423c
Ramón Roche <mrpollo@gmail.com>
2013-04-15 15:40:43 -0700

    add default option prop to model

--- app/models/spree/option_value_decorator.rb
+++ app/models/spree/option_value_decorator.rb
@@ -1,6 +1,6 @@
 Spree::OptionValue.class_eval do
   belongs_to :variant
-  attr_accessible :variant_id, :special_price, :quantity
+  attr_accessible :variant_id, :special_price, :quantity, :default_option
 
   def is_a_variant?
     !variant_id.blank?
