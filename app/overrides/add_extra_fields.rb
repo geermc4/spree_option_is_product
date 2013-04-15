b3ba5004c6a3e7442182ef0def66e48cdedad9b5
Ramón Roche <mrpollo@gmail.com>
2013-04-15 15:41:03 -0700

    add default option to model

--- app/overrides/add_extra_fields.rb
+++ app/overrides/add_extra_fields.rb
@@ -1,4 +1,4 @@
-fields=['quantity','special_price']
+fields=['quantity','special_price', 'default_option']
 
 fields.each do |f|
   Deface::Override.new(
@@ -11,5 +11,5 @@ fields.each do |f|
     :virtual_path   => "spree/admin/option_types/_option_value_fields",
     :name           => "add_#{f}_field",
     :insert_before  => ".actions",
-    :text           => "<td><%= f.text_field :#{f} %> </td>")
+    :text           => "<td> #{(f == 'default_option') ? "<%= f.check_box :#{f} %>" : "<%= f.text_field :#{f} %>"} </td>")
 end
