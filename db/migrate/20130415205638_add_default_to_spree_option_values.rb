fd14a407012f4d3c69e31f833ef49b591de2ea4b
Ramón Roche <mrpollo@gmail.com>
2013-04-15 14:14:33 -0700

    changed name to default_option

--- db/migrate/20130415205638_add_default_to_spree_option_values.rb
+++ db/migrate/20130415205638_add_default_to_spree_option_values.rb
@@ -1,5 +1,5 @@
 class AddDefaultToSpreeOptionValues < ActiveRecord::Migration
   def change
-    add_column :spree_option_values, :default, :boolean
+    add_column :spree_option_values, :default_option, :boolean
   end
 end
