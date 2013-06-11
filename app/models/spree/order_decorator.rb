79ddc2e8e8268e110533d4349a16d7ecc08d9b5b
geermc4 <geermc4@gmail.com>
2013-06-11 15:56:31 -0700

    fixes for 2.0

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Order.class_eval do
-  def find_line_item_by_variant(variant)
+  def find_line_item_parent(variant)
     line_items.detect { |line_item| (line_item.variant_id == variant.id) && line_item.parent.blank? && line_item.children.blank? }
   end
 end
--- app/overrides/change_variant_display.rb
+++ app/overrides/change_variant_display.rb
@@ -9,6 +9,6 @@ new_variant_display = %(
 Deface::Override.new(
   :virtual_path   => "spree/products/_cart_form",
   :name           => "change_variant_display",
-  :surround       => "code[erb-silent]:contains('if @product.has_variants?')",
+  :surround       => "code[erb-silent]:contains('if @product.variants_and_option_values(current_currency).any?')",
   :text           => new_variant_display,
   :original       => 'da5b4e18e3090d127619485fd6d49e3d02b32a6c')
