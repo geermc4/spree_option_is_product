7715793b3b02b95a9afe78714094830036d32473
Ramón Roche <mrpollo@gmail.com>
2013-07-18 16:35:30 -0700

    fix price for kits that have no required options fixes undefined method `+' for nil:NilClass

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -33,8 +33,10 @@ Spree::Product.class_eval do
     new_price = ( !some_price.empty? ) ? some_price.try(:first) : self.price
     if !self.product_options.nil?
       options_price = self.product_options.select{ |po| po[:optional] == false }.map{ |opt, sum| opt[:options].first[:price] }.inject(:+)
-      optional_values_default_pricing = self.product_options.select{ |po| po[:optional] == true }.collect{ |opt| opt[:options] }.collect{ |op| op.select{ |pop| pop[:default_option] == true }.first }.select{ |s| !s.nil? }.select{ |s| !s.empty? }.map{ |v| v[:price] }.inject(:+).to_f
-      new_price += (options_price + (optional_values_default_pricing.nil? ? 0 : optional_values_default_pricing))
+      if !options_price.nil?
+        optional_values_default_pricing = self.product_options.select{ |po| po[:optional] == true }.collect{ |opt| opt[:options] }.collect{ |op| op.select{ |pop| pop[:default_option] == true }.first }.select{ |s| !s.nil? }.select{ |s| !s.empty? }.map{ |v| v[:price] }.inject(:+).to_f
+        new_price += (options_price + (optional_values_default_pricing.nil? ? 0 : optional_values_default_pricing))
+      end
     end
     Spree::Money.new(new_price || 0, {:currency => self.currency} )
   end
