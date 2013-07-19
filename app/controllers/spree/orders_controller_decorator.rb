geermc4/spree_option_is_product

commit 79ddc2e8e8268e110533d4349a16d7ecc08d9b5b

fixes for 2.0

app/controllers/spree/orders_controller_decorator.rb
@@ -6,7 +6,7 @@ Spree::OrdersController.class_eval do
 
   def add_children
     unless params["product_options"].blank?
-      parent = current_order.find_line_item_by_variant(Spree::Variant.find(params["variants"].first[0]))
+      parent = current_order.find_line_item_parent(Spree::Variant.find(params["variants"].first[0]))
 
       params["product_options"].each do |k,v|
         next if v.blank?

app/models/spree/order_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Order.class_eval do
-  def find_line_item_by_variant(variant)
+  def find_line_item_parent(variant)
     line_items.detect { |line_item| (line_item.variant_id == variant.id) && line_item.parent.blank? && line_item.children.blank? }
   end
 end

app/overrides/change_variant_display.rb
@@ -9,6 +9,6 @@ new_variant_display = %(
 Deface::Override.new(
   :virtual_path   => "spree/products/_cart_form",
   :name           => "change_variant_display",
-  :surround       => "code[erb-silent]:contains('if @product.has_variants?')",
+  :surround       => "code[erb-silent]:contains('if @product.variants_and_option_values(current_currency).any?')",
   :text           => new_variant_display,
   :original       => 'da5b4e18e3090d127619485fd6d49e3d02b32a6c')

commit 1901a196cd1373a3b1bbdf0ad51c5648dba57536

Fix bug introduced in 79ddc2e8e8
Order#find_line_item_by_variant is used internally by spree she adding a
line_item to an order, we can't stop using that method's name if we want
to detect the correct line_item(cherry picked from commit 4d498a7037ac8cb8a61323e6c8d497596636f469)

app/controllers/spree/orders_controller_decorator.rb
@@ -6,7 +6,7 @@ Spree::OrdersController.class_eval do
 
   def add_children
     unless params["product_options"].blank?
-      parent = current_order.find_line_item_parent(Spree::Variant.find(params["variants"].first[0]))
+      parent = current_order.find_line_item_by_variant(Spree::Variant.find(params["variants"].first[0]))
 
       params["product_options"].each do |k,v|
         next if v.blank?

app/models/spree/order_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Order.class_eval do
-  def find_line_item_parent(variant)
+  def find_line_item_by_variant(variant)
     line_items.detect { |line_item| (line_item.variant_id == variant.id) && line_item.parent.blank? && line_item.children.blank? }
   end
 end