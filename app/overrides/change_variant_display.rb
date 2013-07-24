915d74d3119fe872e5847ed7e5dd3f8eb82824ce
Ruben Ascencio <galateaweb@gmail.com>
2013-07-24 12:45:54 -0700

    Fixes weird patch in override file. Fixes #8

--- app/overrides/change_variant_display.rb
+++ app/overrides/change_variant_display.rb
@@ -1,14 +1,12 @@
 new_variant_display = %(
-  <% if @product.option_types.collect(&:product_based).include?(true) %>
-    <%= render :partial => 'spree/products/selected_options_display'%>
-  <% else %>
-    <%= render_original %>
-  <% end %>
+    <% if @product.option_types.collect(&:product_based).include?(true) %>
+      <%= render :partial => 'spree/products/selected_options_display'%>
+    <% elsif @product.variants_and_option_values(current_currency).any? %>
 )
 
 Deface::Override.new(
   :virtual_path   => "spree/products/_cart_form",
   :name           => "change_variant_display",
-  :surround       => "code[erb-silent]:contains('if @product.variants_and_option_values(current_currency).any?')",
+  :replace        => "code[erb-silent]:contains('if @product.variants_and_option_values(current_currency).any?')",
   :text           => new_variant_display,
   :original       => 'da5b4e18e3090d127619485fd6d49e3d02b32a6c')
