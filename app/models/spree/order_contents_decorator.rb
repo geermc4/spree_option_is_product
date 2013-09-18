758762a09d4709e7887ecd8e971a7eef1a417f28
Ruben Ascencio <galateaweb@gmail.com>
2013-09-18 09:55:51 -0700

    Set quantity default value to 1
    This change was made here: https://github.com/spree/spree/commit/2717e7574d2c230ec113b5f4b19db0bc4f96f1cf
    
    (seems like) they're assuming a value, so we better go with the flow here.

--- app/models/spree/order_contents_decorator.rb
+++ app/models/spree/order_contents_decorator.rb
@@ -1,6 +1,6 @@
 Spree::OrderContents.class_eval do
 
-  def add(variant, quantity, currency=nil, shipment=nil, price=nil, force_new_line_item=false)
+  def add(variant, quantity=1, currency=nil, shipment=nil, price=nil, force_new_line_item=false)
     line_item = force_new_line_item ? nil : order.find_line_item_by_variant(variant)
     add_to_line_item(line_item, variant, quantity, currency, shipment, price)
   end
