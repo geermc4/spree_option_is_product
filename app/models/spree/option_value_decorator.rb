b39166a741881be5d7d0c39e6531457fd96c9501
Ramón Roche <mrpollo@gmail.com>
2013-09-26 15:06:30 -0700

    adding stock state notice on option value display

--- app/models/spree/option_value_decorator.rb
+++ app/models/spree/option_value_decorator.rb
@@ -5,4 +5,17 @@ Spree::OptionValue.class_eval do
   def is_a_variant?
     !variant_id.blank?
   end
+
+  def to_hash
+    {
+      :id             => id,
+      :presentation   => presentation,
+      :quantity       => quantity,
+      :variant_id     => variant_id,
+      :state          => variant.default_stock_state,
+      :available      => variant.default_stock_item.available?,
+      :price          => special_price || variant.price,
+      :default_option => default_option,
+    }
+  end
 end
